6804a819b33d1c07bafac8e196a34017d510b8a3
Ruk6787 <ruqban.ali@gmail.com>
2026-05-21 00:01:32 +0500

    Removed old comments

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ if os.name != "nt":
 try:
     import uwsgi
     HAS_UWSGI = True
-    # uwsgi.lock()
 except ImportError:
     HAS_UWSGI = False
 
@@ -127,7 +126,6 @@ def smtpd_process_lock():
     if os.name != "nt":
         path = "/tmp/light-mail"
         os.makedirs(path, exist_ok=True)
-        # NOTE: Use db.sqlite3 as file to lock and for persistent storage.❌
         lock_file = open("/tmp/light-mail/smtp.lock", "w")
         try:
             fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -139,7 +137,6 @@ def smtpd_process_lock():
 
 if HAS_UWSGI:
     uwsgi.post_fork_hook = smtpd_process_lock
-    # uwsgi.unlock()
 else:
     smtpd_process_lock()
 
